2795c47c31f260e95adcd74c30d27d21a6a47a76
Raul Pacheco <eu@raulpacheco.com.br>
2021-05-22 23:45:27 -0300

    Import HasFactory

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use HasFactory;
+
     protected $table = 'products';
 
     protected $fillable = ['name', 'slug', 'brand_id', 'description', 'stock', 'price'];
