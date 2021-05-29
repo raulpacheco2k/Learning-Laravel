8fccf78c5251633100c56fd50b122329bfb89cb6
Raul Pacheco <eu@raulpacheco.com.br>
2021-05-29 10:53:08 -0300

    Create a table for jobs to process emails

--- app/Mail/SuccessfullyRegistered.php
+++ app/Mail/SuccessfullyRegistered.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class SuccessfullyRegistered extends Mailable
+class SuccessfullyRegistered extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
