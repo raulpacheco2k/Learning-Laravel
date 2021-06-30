41c33b13bab164ffc64bb38511292214152b4d5f
Raul Pacheco <eu@raulpacheco.com.br>
2021-06-29 23:35:12 -0300

    Cria modulo de Brands

---
+++ Modules/Brands/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('brands::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('brands.name') !!}
+    </p>
+@endsection
